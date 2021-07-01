4a2905fa30f1e73f9ae8a9ea164a36f962f18cd2
Ghazali-telecom <73136214+Ghazali-telecom@users.noreply.github.com>
2021-07-01 15:53:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
-from setuptools import find_packages, setup
+import pathlib
+from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
 setup(
-    name='copy_paste',
-    version='1.0',
-    packages=find_packages()
+    name="copy_paste0",
+    version="1.0.0",
+    description="Read the latest Real Python tutorials",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="Real Python",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=["feedparser", "html2text"],
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
 )
