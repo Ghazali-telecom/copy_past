048b987ce5c91ca8729217ff0dfad8d0162b7050
Ghazali-telecom <73136214+Ghazali-telecom@users.noreply.github.com>
2021-07-01 15:40:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'copy past'
+NAME = 'copy_past'
 DESCRIPTION = 'Image segmentation models with pre-trained backbones. PyTorch.'
 URL = 'https://github.com/qubvel/segmentation_models.pytorch'
 EMAIL = 'qubvel@gmail.com'
